7c2bbdd3337e1c61054fe822fe258370f78c0fb1
Mike Pleimann <mike@pleimann.com>
2017-05-13 08:59:35 -0500

    load store data in home component and input to the subcomponents so that those subcomponents have no dependencies on the store making them easier to test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
 import { MoistureChartComponent } from '../moisture-chart';
 import { BatteryComponent } from '../battery';
+import { SignalComponent } from '../signal';
+import { MoistureState, BatteryState, SignalState } from '../_model';
+import { AppState } from 'app/_store';
 
 @Component({
     selector: 'mp-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+    moistureData: Observable<MoistureState>;
+    batteryData: Observable<BatteryState>;
+    signalData: Observable<SignalState>;
 
-    constructor() { }
-
-    ngOnInit() {
+    constructor(private store: Store<AppState>) {
+        this.moistureData = store.select('moisture');
+        this.batteryData = store.select('battery');
+        this.signalData = store.select('signal');
     }
-
 }
