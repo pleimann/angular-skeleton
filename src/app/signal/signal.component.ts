271948e05669e1fbafb92db2a8789c85f9b2c455
Mike Pleimann <mike@pleimann.com>
2017-05-13 08:52:29 -0500

    Add signal component stub so that there is a place to put the wifi signal display

---
+++ src/app/signal/signal.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+    selector: 'mp-signal',
+    templateUrl: './signal.component.html',
+    styleUrls: ['./signal.component.scss']
+})
+export class SignalComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
